a0b78ff6f31e3e3cf92d88b2db3d047a5959969f
Dominik Engelen <119937576+LoLFuchs@users.noreply.github.com>
2025-06-23 22:56:49 +0200

    version angleich

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "332927654"
+    serviceWorkerVersion: "3836998391"
   }
 });
